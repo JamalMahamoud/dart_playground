c548008105d503264e38a21010cc51d5fb097202
jamal <jamalm35@gmail.com>
2023-08-11 18:57:39 +0100

    second commit

--- bin/dart101.dart
+++ bin/dart101.dart
@@ -1,6 +1,10 @@
 import 'package:dart101/dart101.dart' as dart101;
+import 'package:dart101/models/User.dart';
+import 'package:dart101/models/nurse.dart';
 
 void main(List<String> arguments) {
-
+Nurse  nurse =  new Nurse();
 }
+
+
 
